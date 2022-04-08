cd49b6d3d38d2c56ff47b266ea998b89bbac7315
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-09 01:05:21 +0530

    TodoList Refactor

--- src/components/todoPane/todoList.js
+++ src/components/todoPane/todoList.js
@@ -1,6 +1,17 @@
 import Todo from './todo';
+import TodoManager from '../../services/todoManager';
 
-const TodoList = (context) =>
-	context.state.todoList.map((todo) => Todo({ ...context, data: todo }));
+const TodoList = (context) => {
+	const { state } = context;
+	const { filter } = state;
+	const noTodos = TodoManager.hasNoTodos(state.todoList);
+
+	const filteredTodo
+	= TodoManager.filterTodos(state.todoList, filter);
+
+	return noTodos
+		? []
+		: filteredTodo.map((todo) => Todo({ ...context, data: todo }));
+};
 
 export default TodoList;
