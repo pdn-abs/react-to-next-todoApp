0f1c12224a9a3200299f8f4dbf2de4a1933ff03f
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-15 20:07:15 +0530

    Introduced TaskPane

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { React } from 'react';
 import './App.scss';
+import TaskPane from './components/taskPane';
 import TodoPane from './components/todoPane';
 
 const App = (context) => <div className="App" role="App">
 	<div className="mainDiv">
 		<div className="leftDiv"><b>TodoPane</b>{TodoPane(context)}</div>
-		<div className="rightDiv">TaskPane{}</div>
+		<div className="rightDiv"><b>TaskPane</b>{TaskPane(context)}</div>
 	</div>
 </div>;
 
---
+++ src/components/taskPane/index.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+import '../../App.scss';
+
+const TaskPane = () =>
+	<div className="taskPane">
+		<h3>Task</h3>
+		<div>{}</div>
+	</div>;
+
+export default TaskPane;
