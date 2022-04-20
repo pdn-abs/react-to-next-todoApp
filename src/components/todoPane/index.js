b49fc89ffadc4468f4e39585f6bd5a937009cd7f
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-20 20:18:15 +0530

    dusted

--- src/components/todoPane/index.js
+++ src/components/todoPane/index.js
@@ -1,5 +1,4 @@
 import { React } from 'react';
-import '../../App.scss';
 import Input from './input';
 import TodoList from './todoList.js';
 import ToggleAllCheckBox from './toggleAllCheckBox.js';
