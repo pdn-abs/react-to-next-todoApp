ba51fffa3dc5769606ceed993ec2caab022c781d
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-20 19:41:32 +0530

    Removing Console & Comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { React, useEffect } from 'react';
 import './App.scss';
 import TaskPane from './components/taskPane';
@@ -7,7 +6,6 @@ import TaskManager from './services/taskManager.js';
 import Ticker from './services/ticker';
 
 const App = (context) => {
-	console.log(context);
 	useEffect(() => TaskManager.init(context), []);
 	useEffect(() => Ticker.start(context), []);
 	return <div className="App" role="App">
