bb197b7024403eb1eea89feb0ee3d167739bf2f4
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-21 19:10:04 +0530

    Dusted

--- src/components/taskPane/addButton.js
+++ src/components/taskPane/addButton.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { React } from 'react';
 
 const AddButton = (context) => {
--- src/components/taskPane/index.js
+++ src/components/taskPane/index.js
@@ -1,5 +1,4 @@
 import { React } from 'react';
-import '../../App.scss';
 import TaskList from './taskList';
 
 const TaskPane = (context) =>
