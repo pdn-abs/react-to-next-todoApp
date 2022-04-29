53b01b9162dcdd63e628482c7e1b1735e527c756
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-29 22:51:35 +0530

    Test completed for AddTodo function

--- src/services/todoManager.test.js
+++ src/services/todoManager.test.js
@@ -1,5 +1,24 @@
 /* eslint-disable no-empty-pattern */
 import TodoManager from './todoManager.js';
 describe('todoManager', () => {
-	const {} = TodoManager;
+	const { addTodo } = TodoManager;
+
+	test('Add Todo - adds the given todo', () => {
+		const context = {
+			state: { todoList: [{ id: 'MFMULLYR',
+				todo: 'Submit the Code',
+				completed: false }],
+			input: 'Debug the Code' },
+			config: { idLength: 4 },
+
+		};
+		const result = addTodo(context);
+
+		expect(result).toEqual([{ id: 'MFMULLYR',
+			todo: 'Submit the Code',
+			completed: false },
+		{ id: expect.any(String),
+			todo: 'Debug the Code',
+			completed: false }]);
+	});
 });
