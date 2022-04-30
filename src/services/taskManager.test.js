ce75d9573c210666820c36d157cc2b1e0878dedb
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-04-30 12:50:00 +0530

    Test Completed for GetTask Func

--- src/services/taskManager.test.js
+++ src/services/taskManager.test.js
@@ -1,5 +1,15 @@
-/* eslint-disable no-unused-vars */
 import TaskManager from './taskManager';
 describe('taskManager', () => {
 	const { getTask } = TaskManager;
+
+	test('getTask', () => {
+		const context = {
+			config: { idLength: 4 },
+			data: 'Test the code',
+		};
+		const result = getTask(context);
+
+		expect(result).toEqual({ id: expect.any(String),
+			todo: 'Test the code' });
+	});
 });
