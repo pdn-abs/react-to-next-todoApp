e182dbd5304690b4cc5c82c91f1e40bc0b15d8d6
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-05-03 20:06:33 +0530

    Completed Test For AddTodo

--- src/core/actions.test.js
+++ src/core/actions.test.js
@@ -1,12 +1,26 @@
 import actions from './actions';
+import TodoManager from '../services/todoManager';
 
 describe('actions', () => {
-	const { setInput } = actions;
+	const { setInput, addTodo } = actions;
+	const context = Symbol('context');
 
 	test('SetInput- sets the given input', () => {
 		const data = Symbol('input') ;
 		const result = setInput({ data });
 
 		expect(result).toEqual({ input: data });
+	});
+	test('AddTodo- Adds the given to TodoList', () => {
+		const addedTodo = Symbol('addedTodo');
+
+		jest.spyOn(TodoManager, 'addTodo').mockReturnValue(addedTodo);
+
+		const expectation = { input: '', todoList: addedTodo };
+		const result = addTodo(context);
+
+		expect(TodoManager.addTodo)
+			.toHaveBeenCalledWith(context);
+		expect(result).toEqual(expectation);
 	});
 });
