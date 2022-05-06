84f09c9ea7b4fcc3fcaf273fe462e8c04506d0c6
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-05-06 19:33:31 +0530

    Dusted

--- src/services/taskManager.js
+++ src/services/taskManager.js
@@ -1,28 +1,23 @@
 import { rndString } from '@laufire/utils/random';
 
 const TaskManager = {
-	getTask: (context) => {
-		const { config, data: text } = context;
-
-		return { id: rndString(config.idLength),
-			todo: text };
-	},
+	getTask: ({ config, data: text }) => ({
+		id: rndString(config.idLength),
+		todo: text,
+	}),
 
 	init: (context) =>
 		context.actions.setTasks(context.config.tasks.map((task) =>
-			TaskManager.getTask({ ...context, data: task })),),
+			TaskManager.getTask({ ...context, data: task }))),
 
-	removeTask: (context) => {
-		const { state, data } = context;
-
-		return state.taskList.filter((task) => task.id !== data.id);
-	},
+	removeTask: ({ state, data }) =>
+		state.taskList.filter((task) => task.id !== data.id),
 
 	addTask: (context) => {
 		const { config: { maxTaskListLength }, state: { taskList }} = context;
 
 		return taskList.length <= maxTaskListLength
-			? context.state.taskList.concat(TaskManager.getTask(context))
+			? taskList.concat(TaskManager.getTask(context))
 			: taskList;
 	},
 };
