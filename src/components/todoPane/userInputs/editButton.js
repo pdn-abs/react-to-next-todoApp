a4ddb09535a88755d0c7bbd822b53894ebea27b1
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-05-27 20:21:44 +0530

    Test completed for editButton.test

--- src/components/todoPane/userInputs/editButton.js
+++ src/components/todoPane/userInputs/editButton.js
@@ -1,8 +1,10 @@
 import { React } from 'react';
+import TodoManager from '../../../services/todoManager';
 
 const editButton = (context) =>
 	<button
-		disabled={ context.state.input === '' }
+		role="editButton"
+		disabled={ TodoManager.hasInput(context) }
 		onClick={ () => context.actions.editTodo() }
 	>
 		EDIT</button>;
---
+++ src/components/todoPane/userInputs/editButton.test.js
@@ -0,0 +1,54 @@
+import { render, fireEvent } from '@testing-library/react';
+import editButton from './editButton';
+import TodoManager from '../../../services/todoManager';
+
+describe('editButton', () => {
+	const context = {
+		actions: {
+			editTodo: jest.fn(),
+		},
+		state: {
+			input: Symbol('input'),
+		},
+	};
+
+	test('Displays editButton', () => {
+		const component = render(editButton(context))
+			.getByRole('editButton');
+
+		expect(component).toBeInTheDocument();
+	});
+
+	test('Clicking EditButton will edits the todo ', () => {
+		const component = render(editButton(context))
+			.getByRole('editButton');
+
+		fireEvent.click(component);
+
+		expect(context.actions.editTodo).toHaveBeenCalledWith();
+	});
+
+	test('EditButton under disabled condition', () => {
+		jest.spyOn(TodoManager, 'hasInput')
+			.mockReturnValue(true);
+
+		const component = render(editButton(context))
+			.getByRole('editButton');
+
+		expect(component).toBeDisabled();
+		expect(TodoManager.hasInput)
+			.toHaveBeenCalledWith(context);
+	});
+
+	test('EditButton under enabled condition', () => {
+		jest.spyOn(TodoManager, 'hasInput')
+			.mockReturnValue(false);
+
+		const component = render(editButton(context))
+			.getByRole('editButton');
+
+		expect(component).not.toBeDisabled();
+		expect(TodoManager.hasInput)
+			.toHaveBeenCalledWith(context);
+	});
+});
