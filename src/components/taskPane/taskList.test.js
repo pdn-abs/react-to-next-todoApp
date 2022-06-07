9080d5a7793e85019402d9e83e3a6ce003425023
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-06-07 19:55:00 +0530

    test completed for taskList.js

---
+++ src/components/taskPane/taskList.test.js
@@ -0,0 +1,25 @@
+import { range } from '@laufire/utils/collection';
+import { render } from '@testing-library/react';
+import * as Task from './task.js';
+import { React } from 'react';
+import TaskList from './taskList.js';
+import { rndBetween, rndString } from '@laufire/utils/random';
+
+test('TaskList- lists the tasks', () => {
+	const context = {
+		state: {
+			taskList: range(1, rndBetween()).map(Symbol),
+		},
+	};
+
+	jest.spyOn(Task, 'default').mockImplementation(() =>
+		<div key={ rndString() } role="Task"/>);
+
+	const { getAllByRole } = render(TaskList(context));
+
+	context.state.taskList.map((task, index) => {
+		expect(Task.default)
+			.toHaveBeenCalledWith({ ...context, data: task });
+		expect(getAllByRole('Task')[index]).toBeInTheDocument();
+	});
+});
